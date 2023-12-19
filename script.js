70e7f42c229b27f1912a55e31fbff61dd99968ea
Austin Luper <austin.luper@gmail.com>
2023-12-19 16:11:18 -0500

    Added commented notes in the js file, attempting to outine at least some of the functionality of the app.

---
+++ script.js
@@ -0,0 +1,21 @@
+// WRAP LOGIC/FUNCTIONS SO THAT FUNCTIONS DON'T RUN UNTIL
+// AFTER THE DOM HAS ALREADY FINISHED RENDERING
+
+//---- NOTES FOR FUNCTION CREATION ---- //
+
+// Create an array, each index number associated with today, and then 
+// each individual day after today in a 5-day forecast
+//
+// [0] = today, [1] = tomorrow, ... etc.
+
+// Create function/logic that pairs each element in the array to
+// the appropriate HTML element, using the HTML element ID's
+
+// Create a function/functions which call data from the Weather API,
+// gathering the relevant conditions as laid out in the HTML file
+    // May need to parse 5-day forecast out, then apply each parsed
+    // value to the appropriate spot in the array
+
+// Create function/functions that takes the value(s) from the function
+// that gathers data from the Weather API, and pairs them to the
+// appropriate HTML elements
