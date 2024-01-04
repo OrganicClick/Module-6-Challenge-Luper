fd443fbf59d574e9d304e6b68a847d146ab8bdd2
Austin Luper <austin.luper@gmail.com>
2024-01-03 21:58:52 -0500

    changed the var to const for openWeatherKey

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //This variable stores the OpenWeather API Key
-var openWeatherKey ="78f50753dfedd5fed54761976c0adf1e";
+const openWeatherKey ="78f50753dfedd5fed54761976c0adf1e";
 
 
 // WRAP LOGIC/FUNCTIONS SO THAT FUNCTIONS DON'T RUN UNTIL
